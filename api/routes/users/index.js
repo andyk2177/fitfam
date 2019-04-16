a93b433d74b223430970895cd0950bb781239f26
Andy Kay <andykay.me@gmail.com>
2019-04-15 22:51:12 -0400

    begin migrating to mongodb atlas

--- api/routes/users/index.js
+++ api/routes/users/index.js
@@ -3,19 +3,17 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 const json = require("body-parser").json;
-const User = require("../../models/User");
+
+const mongo = require("mongo");
 
 // add some security-related headers to the response
 app.use(helmet());
 app.use(json());
 
 app.get("*", async (req, res) => {
-  try {
-    const users = await User.query();
-    res.send(users);
-  } catch (err) {
-    res.json(err);
-  }
+  const t = Date.now();
+  const db = await mongo();
+  res.end("Connection took " + (Date.now() - t) + "ms");
 });
 
 module.exports = app;
