f3f1ac240d4dc604457b0356ff1fe6722f92bfe1
Andy Kay <andykay.me@gmail.com>
2019-05-06 19:23:32 -0400

    switch back to mysql

--- api/routes/users/index.js
+++ api/routes/users/index.js
@@ -3,17 +3,19 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 const json = require("body-parser").json;
-
-const mongo = require("mongo");
+const User = require("../../models/User");
 
 // add some security-related headers to the response
 app.use(helmet());
 app.use(json());
 
 app.get("*", async (req, res) => {
-  const t = Date.now();
-  const db = await mongo();
-  res.end("Connection took " + (Date.now() - t) + "ms");
+  try {
+    const users = await User.query();
+    res.send(users);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = app;
